9a831f838a96f00dac44dbed52dd6c8862c6a840
Krzysztof Mróz <kmroz97@gmail.com>
2020-02-16 11:00:05 +0100

    First commit

---
+++ src/test/java/pl/akademiacodu/gifs/GifsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiacodu.gifs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
